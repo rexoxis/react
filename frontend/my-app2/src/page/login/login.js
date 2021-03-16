1fd26b4ae1c68b121e169eacbc8e8f73c60ab825
hoon <rexoxis92@hanmail.net>
2021-03-16 23:59:36 +0900

    movie project add moviesPage pagenation

---
+++ frontend/my-app2/src/page/login/login.js
@@ -0,0 +1,50 @@
+import React, {Component} from "react";
+import axios from "axios";
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading : false,
+            result : ""
+        };
+    }
+
+    loginCheck = async () => {
+
+        axios
+            .post('/login', {
+                data : {
+                    userid : this.props.userid,
+                    passwd : this.props.passwd
+                },
+            })
+            .then(({data}) => {
+                this.setState({
+                    loading: true,
+                    result: data,
+                });
+                console.log("login result : " + data);
+            })
+            .catch(e => {
+                console.error(e);
+                this.setState({
+                    loading: false
+                });
+            });
+    };
+
+    componentDidMount() {
+        this.loginCheck();
+    }
+
+    render() {
+        const result = this.state.result;
+        return(
+            <>
+                {result}
+            </>
+        );
+    }
+}
+export default Login;
