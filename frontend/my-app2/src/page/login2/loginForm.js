22721f303544c6a0f83fb99db4fa6232b98d55e2
hoon <rexoxis92@hanmail.net>
2021-03-26 22:01:29 +0900

    add login

---
+++ frontend/my-app2/src/page/login2/loginForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class LoginForm extends React.Component {
+
+    render() {
+        return(
+            <div className="main">
+                <div className="loginid">
+                    <span>아이디</span>
+                    <input type="text" id="loginUserid"/>
+                </div>
+                <div className="loginpwd">
+                    <span>비밀번호</span>
+                    <input type="password" id="loginPasswd"/>
+                </div>
+                <div className="loginbtn">
+                    <button type="button" id="loginSubmit">로그인</button>
+                    <button type="button" id="loginCancle">취소하기</button>
+                </div>
+            </div>
+        );
+    }
+
+}
+export default LoginForm;
